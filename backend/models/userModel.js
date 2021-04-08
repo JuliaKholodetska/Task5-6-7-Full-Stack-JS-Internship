db23b3c169ec8d6524c772286bf29eb776d3ac8e
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2021-04-08 12:27:51 +0300

    update master

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import sequelizeDB from "../sequelize.js";
-=======
 import sequelizeDB from "../db.js";
->>>>>>> 67cf5b1ab21fbb3e1422ddc45631ea8260a395e0
 import dataTypes from "sequelize";
 import Order from "./orderModel.js";
 import Rating from "./ratingModel.js";
