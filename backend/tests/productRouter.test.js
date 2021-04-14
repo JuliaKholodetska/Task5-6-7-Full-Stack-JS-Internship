65899349a8d210e717d01cc37f78cf985c47f922
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2021-04-14 10:00:33 +0300

    set up env

---
+++ backend/tests/productRouter.test.js
@@ -0,0 +1,10 @@
+// import request from "supertest";
+// import app from "../server.js";
+
+// describe("Product API", () => {
+// 	it("should show all products", async () => {
+// 		const res = await request(app).get("/api/products");
+// 		expect(res.statusCode).toEqual(200);
+// 		// expect(res.body).toHaveProperty('products')
+// 	});
+// });
