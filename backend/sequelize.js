69f74fe973f92f9695cb47305d0c01a6b34d0be9
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2021-05-11 11:37:50 +0300

    update connection db

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -1,26 +1,24 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
-const sequelizeDB = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
+const sequelizeDB = new Sequelize({
+	database: process.env.DB_NAME,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 
-	{
-		// connectionString: process.env.DATABASE_URL,
-		ssl: true,
-		dialect: "postgres",
-		сonnectionSsl: true,
-		dialectOptions: {
-			ssl: {
-				require: true,
-				rejectUnauthorized: false,
-			},
+	// connectionString: process.env.DATABASE_URL,
+	ssl: true,
+	dialect: "postgres",
+	сonnectionSsl: true,
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
 		},
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		logging: false,
-	}
-);
+	},
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	logging: false,
+});
 
 export default sequelizeDB;
