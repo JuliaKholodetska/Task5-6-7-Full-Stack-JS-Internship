3309c4aee4b7a840207a785565aedcfe5324c21e
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2021-05-11 14:14:58 +0300

    in progress

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -1,21 +1,22 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_HOST);
 const sequelizeDB = new Sequelize({
-	// database: process.env.DB_NAME,
-	// username: process.env.DB_USER,
-	// password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 
-	connectionString: process.env.HEROKU_POSTGRESQL_BLUE_URL,
-	ssl: true,
+	// connectionString: process.env.HEROKU_POSTGRESQL_BLUE_URL,
+	// ssl: true,
 	dialect: "postgres",
-	сonnectionSsl: true,
-	dialectOptions: {
-		ssl: {
-			require: true,
-			rejectUnauthorized: false,
-		},
-	},
+	// сonnectionSsl: true,
+	// dialectOptions: {
+	// 	ssl: {
+	// 		require: true,
+	// 		rejectUnauthorized: false,
+	// 	},
+	// },
 
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
