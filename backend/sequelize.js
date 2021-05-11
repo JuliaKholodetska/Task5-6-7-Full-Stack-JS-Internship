7c2344af703441561bfed7fdb9ae01c33bf4944f
JuliaKholodetska <55590609+JuliaKholodetska@users.noreply.github.com>
2021-05-11 10:35:49 +0300

    update connection db now

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 const sequelizeDB = new Sequelize(
-	// process.env.DB_NAME,
+	process.env.DB_NAME,
 	// process.env.DB_USER,
 	// process.env.DB_PASSWORD,
 
